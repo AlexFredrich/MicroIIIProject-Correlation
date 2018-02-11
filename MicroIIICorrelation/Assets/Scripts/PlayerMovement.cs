970e73dbb419ad80c6c8e90d302bc81180ce4b7d
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2018-02-10 21:37:15 -0600

    Scripts and Clean Up
    I wrote up the 2 player scripts and the switches. All of the level design is organized and I incoporated the switching of objects on and off.

---
+++ MicroIIICorrelation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //SerialeFields for editing
+    //Player movement speed
+    [SerializeField] float speed = 5f;
+    //Player jumpp height
+    [SerializeField] float jumpHeight = 5f;
+    //To check if the player is on the ground
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    float groundRadius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    //Player identification string
+    [SerializeField] string playerID = "P1";
+
+    //private fields
+    //Horizontal input
+    float horizontalInput;
+    //Vertical input
+    float verticalInput;
+    //Player rigidbody
+    Rigidbody2D rigidbody;
+    //Name of the horizontal axis
+    string horizontalAxis;
+    //Name of the vertical axis
+    string verticalAxis;
+    private bool grounded;
+    private bool pressedButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Set the axis and buttons names to allow the code to work for both players
+        horizontalAxis = string.Format("{0}-Horizontal", playerID);
+        verticalAxis = string.Format("{0}-Vertical", playerID);
+
+        //Getting reference to the rigidbody
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        UpdateIsOnGround();
+        Jump();
+	}
+
+    private void UpdateIsOnGround()
+    {
+
+        Collider2D[] groundColliders = Physics2D.OverlapCircleAll(groundCheck.position, groundRadius, whatIsGround);
+        grounded = groundColliders.Length > 0;
+    }
+    
+    //Checking the input and moving the player
+    private void Move()
+    {
+        horizontalInput = Input.GetAxis(horizontalAxis);
+        
+        rigidbody.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, rigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        //verticalInput = Input.GetAxis(verticalAxis);
+        pressedButton = Input.GetButton(verticalAxis);
+        if(pressedButton && grounded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpHeight * Time.deltaTime);
+        }
+
+    }
+}
---
+++ MicroIIICorrelation/Assets/Scripts/Switches.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour
+{
+
+    //Get the object that it is interacting with
+    [SerializeField]
+    GameObject interactableObject;
+
+    //Either reveals or removes the object that the switch is connected to
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if (interactableObject.activeSelf == true)
+            {
+                interactableObject.SetActive(false);
+            }
+            else
+            {
+                interactableObject.SetActive(true);
+            }
+        }
+    }
+
+
+
+}
